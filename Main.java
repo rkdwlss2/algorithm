bd6d41319541036c9e94480b7b1006921fc838b8
rkdwlss2 <rkdwlss2@gmail.com>
2020-09-22 22:42:15 +0900

    hi

---
+++ Main.java
@@ -0,0 +1,20 @@
+package inha;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		Integer[] arr = new Integer[n];
+		for (int i = 0 ; i < n ; i++) {
+			arr[i]=sc.nextInt();
+		}
+		Arrays.sort(arr);
+		int ans = 0;
+		for (int i =0 ; i < n ; i++) {
+			ans+=arr[i]*(n-i);
+		}
+		System.out.println(ans);
+	}
+	
+}
